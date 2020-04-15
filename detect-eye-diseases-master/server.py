e565614f2a2e768b29b146443a54f3bc559ae3fd
unknown <christian.stuhlberger@gmx.at>
2020-04-15 09:30:23 +0200

    some changes

--- detect-eye-diseases-master/server.py
+++ detect-eye-diseases-master/server.py
@@ -11,10 +11,10 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from starlette.routing import Route
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1U6vmC0eY_ejOvFvHIjXUsvI7Jsn31SRd'
+export_file_url = 'https://drive.google.com/u/0/uc?id=1WYfn_ABdsy1yOKGuCbcThVCWSQmFSFWT&export=download'
 export_file_name = 'export.pkl'
 
-classes = ['cataract', 'glaucoma', 'normal', 'retina_disease']
+classes = ['deskchairs', 'directorchairs', 'ladderbackchairs', 'stools', 'widsorchairs']
 path = Path(__file__).parent
 
 templates = Jinja2Templates(directory=str('app/templates'))
